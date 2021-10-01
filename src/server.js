e128d545b7e99978fcb1a22e9d6565c01cfb490a
derekzx <chinzhixian@gmail.com>
2021-10-02 00:29:20 +0800

    Fix errors

--- src/server.js
+++ src/server.js
@@ -25,49 +25,56 @@ app.get('/', (req, res) => {
 // Returns associated limit orders for orderer address
 app.get('/liquidity/:tokenA/:tokenB', async (req, res) => {
 
-  const tokenAAddress = req.params.tokenA;
-  const tokenBAddress = req.params.tokenB;
+  try {
+    const tokenAAddress = req.params.tokenA;
+    const tokenBAddress = req.params.tokenB;
+  
+    const tokenAOptions = {
+      address: tokenAAddress,
+      chain: "bsc",
+      exchange: "PancakeSwapv2"
+    };
+  
+    const tokenADetails = await Moralis.Web3API.token.getTokenPrice(tokenAOptions);
+  
+    const tokenBOptions = {
+      address: tokenBAddress,
+      chain: "bsc",
+      exchange: "PancakeSwapv2"
+    };
+  
+    const tokenBDetails = await Moralis.Web3API.token.getTokenPrice(tokenBOptions);
+  
+    const tokenAABI = await getABI(tokenAAddress)
+    const tokenAContract = await new web3.eth.Contract(JSON.parse(tokenAABI), tokenAAddress)
+    const tokenADecimals = await tokenAContract.methods.decimals().call();
+  
+    const tokenBABI = await getABI(tokenBAddress)
+    const tokenBContract = await new web3.eth.Contract(JSON.parse(tokenBABI), tokenBAddress)
+    const tokenBDecimals = await tokenBContract.methods.decimals().call();
+  
+    const pancakeSwapFactoryV2ABI = await getABI(pancakeSwapFactoryAddressV2)
+  
+    const pancakeSwapFactoryV2Contract = new web3.eth.Contract(JSON.parse(pancakeSwapFactoryV2ABI), pancakeSwapFactoryAddressV2)
+  
+    const tokenPairAddress = await pancakeSwapFactoryV2Contract.methods.getPair(tokenAAddress, tokenBAddress).call()
+  
+    const tokenPairABI = await getABI(tokenPairAddress)
+  
+    const tokenPairContract = await new web3.eth.Contract(JSON.parse(tokenPairABI), tokenPairAddress)
+  
+    const tokenPairLiquidity = await tokenPairContract.methods.getReserves().call();
+  
+    const tokenAValue = tokenADetails.usdPrice * tokenPairLiquidity._reserve0 / 10 ** tokenADecimals 
+    const tokenBValue = tokenBDetails.usdPrice * tokenPairLiquidity._reserve1 / 10 ** tokenBDecimals 
+  
+    res.send(JSON.stringify(tokenAValue + tokenBValue));
+  } catch (error) {
+    console.error(error);
+    res.send(JSON.stringify("Server error. Please try again"));
+  }
 
-  const tokenAOptions = {
-    address: tokenAAddress,
-    chain: "bsc",
-    exchange: "PancakeSwapv2"
-  };
-
-  const tokenADetails = await Moralis.Web3API.token.getTokenPrice(tokenAOptions);
-
-  const tokenBOptions = {
-    address: tokenBAddress,
-    chain: "bsc",
-    exchange: "PancakeSwapv2"
-  };
-
-  const tokenBDetails = await Moralis.Web3API.token.getTokenPrice(tokenBOptions);
-
-  const tokenAABI = await getABI(tokenAAddress)
-  const tokenAContract = await new web3.eth.Contract(JSON.parse(tokenAABI), tokenAAddress)
-  const tokenADecimals = await tokenAContract.methods.decimals().call();
-
-  const tokenBABI = await getABI(tokenBAddress)
-  const tokenBContract = await new web3.eth.Contract(JSON.parse(tokenBABI), tokenBAddress)
-  const tokenBDecimals = await tokenBContract.methods.decimals().call();
-
-  const pancakeSwapFactoryV2ABI = await getABI(pancakeSwapFactoryAddressV2)
-
-  const pancakeSwapFactoryV2Contract = new web3.eth.Contract(JSON.parse(pancakeSwapFactoryV2ABI), pancakeSwapFactoryAddressV2)
-
-  const tokenPairAddress = await pancakeSwapFactoryV2Contract.methods.getPair(tokenAAddress, tokenBAddress).call()
-
-  const tokenPairABI = await getABI(tokenPairAddress)
-
-  const tokenPairContract = await new web3.eth.Contract(JSON.parse(tokenPairABI), tokenPairAddress)
-
-  const tokenPairLiquidity = await tokenPairContract.methods.getReserves().call();
-
-  const tokenAValue = tokenADetails.usdPrice * tokenPairLiquidity._reserve0 / 10 ** tokenADecimals 
-  const tokenBValue = tokenBDetails.usdPrice * tokenPairLiquidity._reserve1 / 10 ** tokenBDecimals 
-
-  res.send(JSON.stringify(tokenAValue + tokenBValue));
+  
 
 })
 
