293958bf5ea8099867705e15643841bca0b24774
derekzx <chinzhixian@gmail.com>
2021-10-01 14:19:45 +0800

    Calculate full liquidity price

--- src/server.js
+++ src/server.js
@@ -5,8 +5,13 @@ const Web3 = require("web3")
 const web3 = new Web3("https://bsc-dataseed.binance.org/");
 const axios = require('axios')
 const app = express()
-const cors = require ('cors')
+const cors = require('cors')
 const port = process.env.PORT
+
+const Moralis = require('moralis/node');
+
+Moralis.initialize("GE5JyXV3vDB89irEPsgb3DSdYbiw4XYRzeHgySm2");
+Moralis.serverURL = 'https://zbconyrwn1pv.moralishost.com:2053/server'
 
 const pancakeSwapFactoryAddressV2 = "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73";
 
@@ -18,27 +23,52 @@ app.get('/', (req, res) => {
 })
 
 // Returns associated limit orders for orderer address
-app.get('/liquidity/:tokenA/:tokenB', async(req, res) => {
+app.get('/liquidity/:tokenA/:tokenB', async (req, res) => {
 
-  var pancakeSwapFactoryV2ABI = await getABI(pancakeSwapFactoryAddressV2)
-    
-  var pancakeSwapFactoryV2Contract = new web3.eth.Contract(JSON.parse(pancakeSwapFactoryV2ABI), pancakeSwapFactoryAddressV2)
+  const tokenAAddress = req.params.tokenA;
+  const tokenBAddress = req.params.tokenB;
 
-  var tokenPairAddress = await pancakeSwapFactoryV2Contract.methods.getPair(req.params.tokenA, req.params.tokenB).call()
+  const tokenAOptions = {
+    address: tokenAAddress,
+    chain: "bsc",
+    exchange: "PancakeSwapv2"
+  };
 
-  var tokenPairABI = await getABI(tokenPairAddress)
+  const tokenADetails = await Moralis.Web3API.token.getTokenPrice(tokenAOptions);
 
-  var tokenPairContract = await new web3.eth.Contract(JSON.parse(tokenPairABI), tokenPairAddress)
+  const tokenBOptions = {
+    address: tokenBAddress,
+    chain: "bsc",
+    exchange: "PancakeSwapv2"
+  };
 
-  var tokenPairLiquidity = await tokenPairContract.methods.getReserves().call();
+  const tokenBDetails = await Moralis.Web3API.token.getTokenPrice(tokenBOptions);
 
-  console.log(tokenPairLiquidity);
-  res.send({
-    tokenA: tokenPairLiquidity._reserve0,
-    tokenB: tokenPairLiquidity._reserve1,
-  });
+  const tokenAABI = await getABI(tokenAAddress)
+  const tokenAContract = await new web3.eth.Contract(JSON.parse(tokenAABI), tokenAAddress)
+  const tokenADecimals = await tokenAContract.methods.decimals().call();
 
-  
+  const tokenBABI = await getABI(tokenBAddress)
+  const tokenBContract = await new web3.eth.Contract(JSON.parse(tokenBABI), tokenBAddress)
+  const tokenBDecimals = await tokenBContract.methods.decimals().call();
+
+  const pancakeSwapFactoryV2ABI = await getABI(pancakeSwapFactoryAddressV2)
+
+  const pancakeSwapFactoryV2Contract = new web3.eth.Contract(JSON.parse(pancakeSwapFactoryV2ABI), pancakeSwapFactoryAddressV2)
+
+  const tokenPairAddress = await pancakeSwapFactoryV2Contract.methods.getPair(tokenAAddress, tokenBAddress).call()
+
+  const tokenPairABI = await getABI(tokenPairAddress)
+
+  const tokenPairContract = await new web3.eth.Contract(JSON.parse(tokenPairABI), tokenPairAddress)
+
+  const tokenPairLiquidity = await tokenPairContract.methods.getReserves().call();
+
+  const tokenAValue = tokenADetails.usdPrice * tokenPairLiquidity._reserve0 / 10 ** tokenADecimals 
+  const tokenBValue = tokenBDetails.usdPrice * tokenPairLiquidity._reserve1 / 10 ** tokenBDecimals 
+
+  res.send(JSON.stringify(tokenAValue + tokenBValue));
+
 })
 
 app.get('/health', (req, res) => res.send("Healthy"));
@@ -50,10 +80,10 @@ app.listen(port, () => {
 async function getABI(address) {
   const res = await axios.get('https://api.bscscan.com/api', {
     params: {
-        module: 'contract',
-        action: 'getabi',
-        address: address,
-        apiKey: 'IEXFMZMTEFKY351A7BG72V18TQE2VS74J1',
+      module: 'contract',
+      action: 'getabi',
+      address: address,
+      apiKey: 'IEXFMZMTEFKY351A7BG72V18TQE2VS74J1',
     },
   })
   return res.data.result
